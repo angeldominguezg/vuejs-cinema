04173a9dfe74c51e46b40fc749b1edb0483efb5e
Angelddg <angeldominguezg@gmail.com>
2018-05-07 18:48:52 -0400

    Adding api data and updating the movie filters

---
+++ src/components/MovieFilter.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="movie-filter">
+    <h2>Filter results</h2>
+    <div class="filter-group">
+      <check-filter v-for="genere in genres" v-bind:title="genere" v-on:check-filter="checkFilter"></check-filter>
+    </div>
+  </div>
+</template>
+
+<script>
+import genres from '../util/genres'
+
+export default {
+  data() {
+    return {
+      genres
+    }
+  },
+  methods: {
+    checkFilter(category, title, checked) {
+      // console.log('[checkFilter]')
+      this.$emit('check-filter', category, title, checked)
+    }
+  },
+  components: {
+    'check-filter': {
+      data() {
+        return {
+          checked: false
+        }
+      },
+      props: ['title'],
+      template: `<div v-bind:class="{'check-filter': true, active: checked}" v-on:click="checkFilter">
+                    <span class="checkbox"></span>
+                    <span class="check-filter-title">{{ title }}</span>
+                </div>`,
+      methods: {
+        checkFilter() {
+          this.checked = !this.checked
+          this.$emit('check-filter', 'genre', this.title, this.checked)
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,41 +1,44 @@
 import './style.scss'
 import Vue from 'vue'
-import generes from './util/genres'
+import MovieList from './components/MovieList.vue'
+import MovieFilter from './components/MovieFilter.vue'
+
+import vueResource from 'vue-resource'
+Vue.use(vueResource)
 
 new Vue({
   el: '#app',
-  components: {
-    'movie-list': {
-      template: `<div id="movie-list">
-                    <div v-for="movie in movies" class="movie">{{ movie.title }}</div>
-                  </div>`,
-      data() {
-        return {
-          movies: [
-            { title: 'Pulp Fiction' },
-            { title: 'Home Alone' },
-            { title: 'Austin Powers' }
-          ]
-        }
-      }
-    },
-    'movie-filter': {
-      data () {
-        return {
-          generes
-        }
-      },
-      template: `<div id="movie-filter">
-                    <h2>Filter result</h2>
-                    <div class="filter-group">
-                      <check-filter v-for="genere in generes"></check-filter>
-                    </div>
-                  </div>`,
-      components: {
-        'check-filter': {
-          template: `<div>Filter</div>`
+  data: {
+    time: [],
+    genre: [],
+    movies: []
+  },
+  methods: {
+    checkFilter(category, title, checked) {
+      console.log('[checkFilter]: ', category, title, checked)
+      if (checked) {
+        this[category].push(title)
+      } else {
+        let index = this[category].indexOf(title)
+        if (index > -1) {
+          this[category].splice(index, 1)
         }
       }
     }
+  },
+  components: {
+    MovieList,
+    MovieFilter
+  },
+  created() {
+    this.$http
+      .get('/api')
+      .then(response => {
+        console.log('[created]', response.data)
+        this.movies = response.data
+      })
+      .catch(error => {
+        console.log('[created]', error)
+      })
   }
 })
